b643265528d641e82291305c83d654d81a92e841
Vladimir Tsimbol <vtsimbol@gmail.com>
2022-02-14 03:54:28 +0500

    Moved wayfinder to core directory

--- core/finder.py
+++ core/finder.py
@@ -1,13 +1,9 @@
-import argparse
+
 import json
 
 import numpy as np
 
 from utils import Visualizer
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--input_file', type=str, required=True, help='The path to the warehouse annotation')
-args = parser.parse_args()
 
 
 class WayFinder:
@@ -72,8 +68,3 @@ class WayFinder:
 
     def __call__(self):
         pass
-
-
-if __name__ == '__main__':
-    finder = WayFinder(anno_path=args.input_file)
-    finder()
---
+++ find_way.py
@@ -0,0 +1,12 @@
+import argparse
+
+from core import WayFinder
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file', type=str, required=True, help='The path to the warehouse annotation')
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    finder = WayFinder(anno_path=args.input_file)
+    finder()
