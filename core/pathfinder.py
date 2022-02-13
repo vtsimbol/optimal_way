ff7e6fb1f223d71a8d3965358e470915355e3c27
Vladimir Tsimbol <vtsimbol@gmail.com>
2022-02-14 04:15:30 +0500

    Added checking of point to pathfinder

--- core/pathfinder.py
+++ core/pathfinder.py
@@ -8,4 +8,5 @@ class Pathfinder:
     def find_way(self, point1, point2):
         x0, y0 = point1
         x1, y1 = point2
-
+        if self._stacks_mask[x0, y0] == 1 or self._stacks_mask[x1, y1] == 1:
+            raise ValueError('Incorrect point')
