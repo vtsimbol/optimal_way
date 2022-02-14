289e612fdf72c4a60fa55acf42a2d58b6ce1eb9e
Vladimir Tsimbol <vtsimbol@gmail.com>
2022-02-15 01:53:35 +0500

    Added BFS to pathfinder

--- core/pathfinder.py
+++ core/pathfinder.py
@@ -1,19 +1,25 @@
 import numpy as np
 
-from way import PointType, Way
-
 
 class Pathfinder:
     def __init__(self, stacks_mask: np.ndarray):
         self._stacks_mask = stacks_mask
+        self._found_ways = []
 
-    @staticmethod
-    def _get_direction(point1, point2):
+    def start(self, point1, point2):
         x0, y0 = point1
         x1, y1 = point2
-        direct = np.asarray([x1 - x0, y1 - y0], dtype=np.float)
-        direct /= np.linalg.norm(direct)
-        return direct
+        if self._stacks_mask[x0, y0] == 1 or self._stacks_mask[x1, y1] == 1:
+            raise ValueError('Incorrect point')
+
+        self._found_ways = []
+        previous_steps_mask = self._stacks_mask.copy()
+        self._find_way(point1, point2, previous_steps_mask, [])
+
+        number_of_points = [len(w) for w in self._found_ways]
+        best_way_idx = np.argmin(number_of_points)
+        best_way = self._found_ways[best_way_idx]
+        return self._found_ways, best_way
 
     def _get_neighboring_points(self, point, used_points_mask: np.ndarray):
         xc, yc = point
@@ -26,7 +32,7 @@ class Pathfinder:
         roi = available_pts[x0:x1 + 1, y0:y1 + 1]
 
         x_indices, y_indices = np.where(roi)
-        points = [(x + x0, y + y0) for x, y in zip(x_indices, y_indices) if not (x + x0 == xc and y + y0 == yc)]
+        points = [(x + x0, y + y0) for x, y in zip(x_indices, y_indices)]
         return points
 
     @staticmethod
@@ -64,58 +70,43 @@ class Pathfinder:
                     x1, y1 = x0, y0 + 1
         return x1, y1
 
-    def find_way(self, point1, point2):
-        x0, y0 = point1
-        x1, y1 = point2
-        if self._stacks_mask[x0, y0] == 1 or self._stacks_mask[x1, y1] == 1:
-            raise ValueError('Incorrect point')
+    def _find_way(self, start_point, finish_point, previous_steps_mask, way: list):
+        way.append(start_point)
 
-        used_points_mask = self._stacks_mask.copy()
+        if start_point[0] == finish_point[0] and start_point[1] == finish_point[1]:
+            self._found_ways.append(way)
+            return
 
-        way = Way()
-        while True:
-            if point1[0] == point2[0] and point1[1] == point2[1]:
-                print('Finish!')
-                break
+        x0, y0 = start_point
+        previous_steps_mask[x0, y0] = True
 
-            direction = self._get_direction(point1, point2)
-            available_points = self._get_neighboring_points(point1, used_points_mask)
-            if available_points is None:
-                raise RuntimeError('Points not found')
+        available_points = self._get_neighboring_points(start_point, previous_steps_mask)
+        if available_points is None:
+            return
 
-            x, y = self._get_next_step(point1, direction, short_way=True)
-            if (x, y) in available_points:
-                print(f'({x}, {y})')
-                way.add_point(cords=(x, y), point_type=PointType.STEP)
+        for point in available_points:
+            x, y = point
 
-                x0, y0 = point1
-                used_points_mask[x0, y0] = True
-                point1 = (x, y)
+            if not ((np.abs(x - x0) == 1 and np.abs(y - y0) == 0) or
+                    (np.abs(x - x0) == 0 and np.abs(y - y0) == 1)):
                 continue
 
-            x, y = self._get_next_step(point1, direction, short_way=False)
-            if (x, y) in available_points:
-                print(f'({x}, {y})')
-                way.add_point(cords=(x, y), point_type=PointType.STEP)
-
-                x0, y0 = point1
-                used_points_mask[x0, y0] = True
-                point1 = (x, y)
-                continue
-
-            raise RuntimeError('LOL!')
+            previous_steps_mask[x, y] = True
+            self._find_way(point, finish_point, previous_steps_mask.copy(), way.copy())
 
 
 if __name__ == '__main__':
     stacks_mask = np.asarray([
         [False, False, False, False, False],
-        [False, True, True, True, False],
-        [False, True, False, True, False],
+        [False, False, True, False, False],
+        [False, False, True, False, False],
         [False, False, False, False, False],
-        [False, False, True, True, True]
+        [False, True, False, True, False],
+        [False, True, False, True, False],
+        [False, False, False, False, False]
     ], dtype=bool)
 
-    point1, point2 = (4, 0), (0, 4)
+    point1, point2 = (0, 0), (6, 4)
 
     pathfinder = Pathfinder(stacks_mask=stacks_mask)
-    pathfinder.find_way(point2, point1)
+    pathfinder.start(point1, point2)
